074851fcfaa7f0242088c8d8d2793a2186cfa7b2
sa7ik <mohammedsabikoo7@gmail.com>
2024-10-23 11:43:53 +0530

    updated

---
+++ 2704. To Be Or Not To Be.js
@@ -0,0 +1,13 @@
+var expect = function(val) {
+    return{
+        toBe:(v)=>{
+            if (v===val) return true;
+            else throw new Error("Not Equal")
+        },
+        notToBe:(v)=>{
+            if (v!==val) return true
+            else throw new Error ("Equal")
+        }   
+    }
+    
+};
